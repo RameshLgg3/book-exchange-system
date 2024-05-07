e1c39d990273a001f75245e595191b980f5fadca
RameshLgg3 <111238882+RameshLgg3@users.noreply.github.com>
2024-05-07 08:58:23 +0530

    db changes

--- config/db.js
+++ config/db.js
@@ -3,10 +3,10 @@ const mysql = require("mysql");
 // MySQL Connection Pool
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: "localhost",
-  user: "root",
-  password: "Rams2000*",
-  database: "book_exchange_system",
+  host: "sql212.infinityfree.com",
+  user: "if0_36492116",
+  password: "bookExchange",
+  database: "if0_36492116_book_exchange",
 });
 
 module.exports = pool;
