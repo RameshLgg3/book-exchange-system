6cac54e3135a9ec6a0ef1170224204dbff4a9a26
RameshLgg3 <111238882+RameshLgg3@users.noreply.github.com>
2024-05-07 09:56:21 +0530

    db

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 // MySQL Connection Pool
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: "sql212.byetcluster.com",
+  host: "sql212.infinityfree.com",
   user: "if0_36492116",
   password: "bookExchange",
   database: "if0_36492116_book_exchange",
