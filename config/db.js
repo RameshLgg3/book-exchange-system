c8324b511379a0f7317ba7ddd04884e564d88af4
RameshLgg3 <111238882+RameshLgg3@users.noreply.github.com>
2024-05-07 20:33:50 +0530

    mysql
    mysql

--- config/db.js
+++ config/db.js
@@ -3,10 +3,10 @@ const mysql = require("mysql");
 // MySQL Connection Pool
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: "sql212.infinityfree.com",
-  user: "if0_36492116",
-  password: "bookExchange",
-  database: "if0_36492116_book_exchange",
+  host: "us-cluster-east-01.k8s.cleardb.net",
+  user: "b091f7e174473a",
+  password: "7b5f16c8",
+  database: "heroku_32d881b5a14dd64",
 });
 
 module.exports = pool;
